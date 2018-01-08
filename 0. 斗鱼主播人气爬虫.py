21ae0dccaa04d53958273a5e431d29579117e46d
Jartus <305190741@qq.com>
2018-01-08 15:07:03 +0800

    fixed

--- 0. 斗鱼主播人气爬虫.py
+++ 0. 斗鱼主播人气爬虫.py
@@ -1,18 +1,24 @@
 #!/usr/bin/python
 # encoding=utf-8
 
+from matplotlib.font_manager import FontProperties
+font = FontProperties(fname='/Library/Fonts/Microsoft/Microsoft Yahei.ttf') #设置中文字体
 import matplotlib
-matplotlib.use('Agg')
+matplotlib.use('Agg')    #不显示图片，直接保存下来，方便扔服务器上跑
 import numpy as np
 import matplotlib.pyplot as plt
 import requests, codecs, time, sys
+reload(sys)
+sys.setdefaultencoding('utf-8')    #中文字体。。。
 from bs4 import BeautifulSoup
-
 
 
 Name = sys.argv[1]
 ISOTIMEFORMAT = '%Y-%m-%d %X'
 DOWNLOAD_URL = 'https://www.douyu.com/directory/game/DOTA2'
+
+def get_time():
+    return time.strftime(ISOTIMEFORMAT, time.localtime())
 
 def parse_html(html):
     soup = BeautifulSoup(html, "html.parser")
@@ -23,7 +29,7 @@ def parse_html(html):
         anchor_name = each.find('span', attrs={'class':'dy-name ellipsis fl'}).getText().strip()
         if Name in anchor_name:
             anchor_popu = each.find('span', attrs={'class':'dy-num fr'}).getText().strip()
-            list_data.append(time.strftime(ISOTIMEFORMAT, time.localtime()) + ', ' + anchor_popu)
+            list_data.append(get_time() + ', ' + anchor_popu)
             return float(anchor_popu[0:-1])
             
     
@@ -40,22 +46,25 @@ def main():
     url = DOWNLOAD_URL
     global list_data
     list_data = []
-    x = 0
-    pic, = plt.plot([], [])
-    plt.xlabel('(mins)', color='g')
-    plt.ylabel('(w)', color='g')
-    plt.title('%s' % Name, color='g')
+    x_data = []
+    y_data = []
+    plt.xlabel('(分钟)', color='g', FontProperties=font)
+    plt.ylabel('(万/人)', color='g', FontProperties=font)
     while True:
         html = download_page(url)
+        x_data.append(get_time()[10:16].strip())
+        x = range(len(x_data))
         y = parse_html(html)
-        pic.set_xdata(np.append(pic.get_xdata(), x))
-        pic.set_ydata(np.append(pic.get_ydata(), y))
+        y_data.append(y)    #加入新的监测数据
+        plt.title('%s %s直播间人数' % (get_time()[:10], Name), color='g', FontProperties=font)
+        plt.xticks(x, x_data, rotation=90)
+        plt.subplots_adjust(bottom=0.15)
         try:
-            plt.plot(x, y)
+            plt.plot(x, y_data)
             plt.savefig('%s.png' % Name)
-        except BaseException as e:
+        except BaseException as e:        #如果主播下播了，就抛出Exception，程序停止
+            print e
             break
-        x += 1
         file_io('%s' % Name, list_data)
         plt.pause(1)
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# python
+# 监测某个斗鱼主播的人气
+##例如，监测甲哥的人气
+![zsmj](../source/ZSMJ.png)
+
